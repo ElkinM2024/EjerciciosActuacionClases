0f7edece3e4a46f439ff7ba9580d2d9ddaeea30c
Elkin Martillo <elkinmartillo1@gmail.com>
2025-02-26 17:06:57 -0500

    Update Program.cs

--- SegundoEjercicio/Program.cs
+++ SegundoEjercicio/Program.cs
@@ -1,15 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace SegundoEjercicio
+public class CuentaBancaria
 {
-    internal class Program
+    private string titular;
+    private decimal saldo;
+
+    public string Titular
     {
-        static void Main(string[] args)
+        get { return titular; }
+        set { titular = value; }
+    }
+
+    public decimal Saldo
+    {
+        get { return saldo; }
+        private set { saldo = value; }
+    }
+
+    public void Depositar(decimal cantidad)
+    {
+        if (cantidad > 0)
         {
+            saldo += cantidad;
+            Console.WriteLine($"Se han depositado {cantidad}.");
         }
+        else
+        {
+            Console.WriteLine("La cantidad debe ser positiva.");
+        }
+    }
+
+    public void Retirar(decimal cantidad)
+    {
+        if (cantidad > 0 && saldo >= cantidad)
+        {
+            saldo -= cantidad;
+            Console.WriteLine($"Se han retirado {cantidad}.");
+        }
+        else
+        {
+            Console.WriteLine("Fondos insuficientes o cantidad inválida.");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        CuentaBancaria cuenta = new CuentaBancaria();
+        cuenta.Depositar(500);
+        cuenta.Retirar(200);
+        Console.WriteLine($"Saldo actual: {cuenta.Saldo}");
     }
 }

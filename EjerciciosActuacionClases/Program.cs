3d4d54220fa9aadc3c247da131967682e5bf34b0
Elkin Martillo <elkinmartillo1@gmail.com>
2025-02-26 17:03:55 -0500

    Update Program.cs

--- EjerciciosActuacionClases/Program.cs
+++ EjerciciosActuacionClases/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace EjerciciosActuacionClases
+public class Coche
 {
-    internal class Program
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public int Año { get; set; }
+    public string Color { get; set; }
+
+    public void Encender()
     {
-        static void Main(string[] args)
-        {
-        }
+        Console.WriteLine("El coche ha sido encendido.");
+    }
+
+    public void Apagar()
+    {
+        Console.WriteLine("El coche ha sido apagado.");
+    }
+
+    public void Informacion()
+    {
+        Console.WriteLine($"Marca: {Marca}, Modelo: {Modelo}, Año: {Año}, Color: {Color}");
     }
 }
+
+class Program
+{
+    static void Main()
+    {
+        Coche coche1 = new Coche() { Marca = "Toyota", Modelo = "Corolla", Año = 2020, Color = "Rojo" };
+        coche1.Informacion();
+        coche1.Encender();
+        coche1.Apagar();
+    }
+}
+
